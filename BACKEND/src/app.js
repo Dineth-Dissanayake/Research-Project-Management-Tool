2768ad3e8973ae090ed3d9b42907b0937c850482
Dineth Dissanayake <77220940+Dineth-Dissanayake@users.noreply.github.com>
2022-05-19 13:36:35 +0530

    INITIAL_PART
    adding server file(app.js)

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || "8050";
+
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res, next) => {
+    res.send("<h2>🈯 Research Project Management Tool</h2>");
+    next();
+})
+
+app.listen(PORT, () => {
+    console.log('🚀 SERVER IS UP AND RUNNING ON PORT :', PORT);
+});
