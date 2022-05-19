d769dc3df9cf3b3b5ac91d311325fc1e330bee39
Dineth Dissanayake <77220940+Dineth-Dissanayake@users.noreply.github.com>
2022-05-19 14:11:06 +0530

    ADD LOGGER
    logger view files add

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from "express";
 import cors from "cors";
+//import logger from './utils/logger';
 
 const app = express();
 const PORT = process.env.PORT || "8050";
