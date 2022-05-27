191c4989f978161860c3cfdd156ab38731c0e39f
Nethmini Gunawardhana <71486601+GunawardhanaHPMN@users.noreply.github.com>
2022-05-27 12:03:08 +0530

    Delete IT20258030AF/server directory

--- IT20258030AF/server/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require("mongoose");
-
-const connectDB =async () =>{
-    return mongoose
-    .connect ("mongodb://localhost/af")
-    .then(()=>console.log(`connection to database estabilished..`))
-    .catch((err) =>console.log(err));
-};
-module.exports=connectDB;
