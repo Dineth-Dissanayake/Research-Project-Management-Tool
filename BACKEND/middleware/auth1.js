427d6094d191b6efbc441f3035e2494cfdf05f49
123kavi <83937185+123kavi@users.noreply.github.com>
2022-05-31 00:51:56 +0530

    middleware create

---
+++ BACKEND/middleware/auth1.js
@@ -0,0 +1,40 @@
+const Iron = require("@hapi/iron");
+const User = require('../model/User1');
+
+const isLoggedIn = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.send({ success: false, message: "Missing authorization token" });
+  } else {
+    const token = authorization.split(" ")[1];
+    if (!token) {
+      res.send({ success: false, message: "Invalid authorization token" });
+    }
+    try {
+      const { email, role } = await Iron.unseal(
+        token,
+        process.env.IRON_KEY,
+        Iron.defaults
+      );
+      const validUser = userExists(email);
+      if (validUser) {
+        req.email = email;
+        req.role = role;
+        next();
+      } else {
+        res.send({ success: false, message: "Invalid authorization token" });
+      }
+    } catch (error) {
+      console.error(error.message);
+      res.send({ success: false, message: "Invalid authorization token" });
+    }
+  }
+};
+
+const userExists = async (email) => {
+  return await User.findOne({email});
+}
+
+module.exports = {
+  isLoggedIn,
+};
